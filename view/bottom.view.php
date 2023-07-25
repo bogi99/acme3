fae54ddf2bd846dfef8e2ebcd19493a0bfd7d23d
sam <khangyi@shaw.ca>
2023-07-25 02:51:23 -0400

    acme3 a new version of acme to replace acme2

---
+++ view/bottom.view.php
@@ -0,0 +1,22 @@
+<div class="fixed bottom-0 left-0 w-full border-t border-gray-700 h-21 bg-slate-700" >
+    <div class="flex flex-col flex-3 " > 
+        <div class="bg-white ">&nbsp;</div>
+        <div class="mx-2 text-gray-200 ">
+            <span class="float-left ">Powered by the mighty Tux, forever reliable and robust</span> 
+            <span class="float-right ">Powered by the mighty Tux, forever reliable and robust</span> 
+        </div>
+        <div class="mx-2 text-gray-400 ">
+            
+            <span class="float-left ">&copy;Copyright 2023 Shop4Web </span> 
+            <span class="float-right text-sm ">
+                <a class="" href="<?php echo listRandomizer::getList('./data/lists.linux.links'); ?>" target="_blank" >Linux</a> 
+                <a class="" href="<?php echo listRandomizer::getList('./data/lists.opensource.links'); ?>" target="_blank" >Open Source</a>
+                <a class="" href="#" >Free Software</a>
+                <a class="" href="#" >HTML5</a>
+                <a class="" href="#" >CSS3</a>
+                <a class="" href="#" >DESIGN</a>
+
+            </span>
+        </div>
+    </div>    
+</div>
